0f88f30bf5c76299b1d288f4b8dda38ced99af8e
Ilya Boyur <i.boyur@idaproject.com>
2023-04-20 16:05:23 +0300

    Update startup

--- src/main.py
+++ src/main.py
@@ -5,33 +5,35 @@ from fastapi import FastAPI
 from fastapi.responses import ORJSONResponse
 
 # Добавляем импорт на ранее созданный модуль
-from api.v1 import base
+from api.v1 import base, di_examples, di_verifications
 from core import config
 from core.config import app_settings
 from core.logger import LOGGING
 
 app = FastAPI(
     # Конфигурируем название проекта. Оно будет отображаться в документации
-    title=app_settings.app_title,   # название приложение берём из настроек
+    title=app_settings.app_title,  # название приложение берём из настроек
     # Адрес документации в красивом интерфейсе
-    docs_url='/api/openapi',
+    docs_url="/api/openapi",
     # Адрес документации в формате OpenAPI
-    openapi_url='/api/openapi.json',
-    # Можно сразу сделать небольшую оптимизацию сервиса 
+    openapi_url="/api/openapi.json",
+    # Можно сразу сделать небольшую оптимизацию сервиса
     # и заменить стандартный JSON-сериализатор на более шуструю версию, написанную на Rust
     default_response_class=ORJSONResponse,
 )
 
 # Подключаем роутер к серверу, указав префикс /v1
-app.include_router(base.router, prefix='/api/v1')
+app.include_router(base.router, prefix="/api/v1")
+app.include_router(di_examples.router, prefix="/api/v1")
+app.include_router(di_verifications.router, prefix="/api/v1")
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # Приложение может запускаться командой
     # `uvicorn main:app --host 0.0.0.0 --port 8080`
     # но чтобы не терять возможность использовать дебагер,
     # запустим uvicorn сервер через python
     uvicorn.run(
-        'main:app',
+        "main:app",
         host=config.PROJECT_HOST,
         port=config.PROJECT_PORT,
         reload=True,
