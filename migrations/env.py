c36c1049c80c9730f284bef4a79ace9111843c85
Ilya Boyur <i.boyur@idaproject.com>
2023-04-21 11:18:53 +0300

    Fix target_metadata

--- migrations/env.py
+++ migrations/env.py
@@ -8,10 +8,10 @@ from sqlalchemy.ext.asyncio import async_engine_from_config
 from alembic import context
 from dotenv import load_dotenv
 
-from src.db.db import Base
-from src.models.entity import Entity
 
-target_metadata = Base.metadata
+
+
+
 
 
 load_dotenv('.env')
@@ -31,9 +31,10 @@ if config.config_file_name is not None:
 
 # add your model's MetaData object here
 # for 'autogenerate' support
-# from myapp import mymodel
-# target_metadata = mymodel.Base.metadata
-target_metadata = None
+from src.db.db import Base
+from src.models.entity import Entity
+
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
